e20d9dd2f65b3543adfd49b8a48bd8481ca46fe2
sa7erpizza <51795900+sa7erpizza@users.noreply.github.com>
2020-06-01 02:56:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    TOKEN: 'توكن',    
+    TOKEN: 'NzE2ODEwMDEwMjA5ODEyNTEx.XtRPPQ.fYsU95X7j82AWX0dEhDrjCY1Ucw',    
     YT_API_KEY: 'api ',
-    prefix: 'برافكس',
-    devs: ['اى دى انت']
+    prefix: '$',
+    devs: ['436868598300934146']
 }
